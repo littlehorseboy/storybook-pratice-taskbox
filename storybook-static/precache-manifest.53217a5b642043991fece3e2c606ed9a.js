e9077050dbc6cbaf0c51228ae0c4a8cdff217bec
horse <littlehorseboy@gmail.com>
2019-09-04 13:46:03 +0800

    build and rename

---
+++ storybook-static/precache-manifest.53217a5b642043991fece3e2c606ed9a.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfce0d01792f5d479820223573655ce4",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.457f1a9757c0815bd01a.bundle.js"
+  },
+  {
+    "revision": "8962f1185d62e9c6c555",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.457f1a9757c0815bd01a.bundle.js"
+  },
+  {
+    "url": "vendors~main.457f1a9757c0815bd01a.bundle.js"
+  }
+]);
